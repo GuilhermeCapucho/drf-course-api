06c4cc705d53600ba498e4e21741398242e28ec3
Guilherme Capucho <guilherme.capucho11@gmail.com>
2026-02-04 11:41:58 -0300

    Lesson 9 (DRF) - class APIView

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
-    path('product/info/', views.product_info),
+    path('product/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
